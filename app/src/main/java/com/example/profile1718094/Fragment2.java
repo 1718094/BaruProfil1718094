8b88e2754501f1ab8dc0919feddf36ea7f792007
1718094 <mdwiarniyanto@gmail.com>
2019-12-31 10:12:42 +0700

    bahasa
    pengaturan bahasa

--- app/src/main/java/com/example/profile1718094/Fragment2.java
+++ app/src/main/java/com/example/profile1718094/Fragment2.java
@@ -7,16 +7,16 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
-public class Fragment1 extends Fragment {
+public class Fragment2 extends Fragment {
 
 
-    public Fragment1() {
+    public Fragment2() {
         // Required empty public constructor
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment1, container, false);
+        return inflater.inflate(R.layout.fragment2, container, false);
     }
 }
